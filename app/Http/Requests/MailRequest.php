0b08d0feefde76cafd40e3c201735d6841cf6b38
Baloululu <bluchier@laposte.net>
2018-12-02 20:57:56 +0100

    Correction captcha

--- app/Http/Requests/MailRequest.php
+++ app/Http/Requests/MailRequest.php
@@ -2,6 +2,7 @@
 
 namespace App\Http\Requests;
 
+use App\Rules\Captcha;
 use Illuminate\Foundation\Http\FormRequest;
 
 class MailRequest extends FormRequest
@@ -26,7 +27,8 @@ class MailRequest extends FormRequest
         return [
             'email' => 'required|email',
             'content' => 'required',
-            'object' => 'required'
+            'object' => 'required',
+            'g-recaptcha-response' => new Captcha()
         ];
     }
 }
